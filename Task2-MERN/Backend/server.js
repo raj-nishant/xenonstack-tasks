241a47d49771019efeff16c66ae81656ec3a51de
Nishant Raj <79581018+raj-nishant@users.noreply.github.com>
2024-01-03 19:40:15 +0530

    update

--- Task2-MERN/Backend/server.js
+++ Task2-MERN/Backend/server.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 // Use CORS middleware
 app.use(cors());
