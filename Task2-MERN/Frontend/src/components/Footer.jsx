cedc746d7730a160f341f1036aea8cae89f6467f
Nishant Raj <79581018+raj-nishant@users.noreply.github.com>
2024-01-04 21:15:50 +0530

    commit

--- Task2-MERN/Frontend/src/components/Footer.jsx
+++ Task2-MERN/Frontend/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-800 text-white p-4 text-center">
+    <footer className="bg-gray-800 text-white p-4 text-center fixed bottom-0 w-full">
       Made with <span className="text-red-500">&#10084;</span> by Nishant
     </footer>
   );
